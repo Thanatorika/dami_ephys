e4a9501331e9cef7ed78e31985892c5792398608
Thanatorika <renwanginmath@gmail.com>
2024-10-18 21:03:11 +0800

    241018

--- stat_measures/firing_rate.py
+++ stat_measures/firing_rate.py
@@ -1,5 +1,5 @@
 """Creator: Ren Wang.
-Last edited: 2024-Oct-16 by RW.
+Last edited: 2024-Oct-18 by RW.
 
 Code utilities for computing firing rates from spike bins.
 """
@@ -19,3 +19,21 @@ def compute_firing_rate_mean(firing_rate):
     # TODO: API
 
     return np.mean(firing_rate, axis=1)
+
+def compute_firing_rate_variance(firing_rate):
+    # TODO: API
+
+    return np.var(firing_rate, axis=1)
+
+def compute_firing_rate_standard_deviation(firing_rate):
+    # TODO: API
+
+    return np.std(firing_rate, axis=1)
+
+def compute_firing_rate_coefficient_of_variance(firing_rate):
+    # TODO: API
+
+    fr_mean = compute_firing_rate_mean(firing_rate)
+    fr_std = compute_firing_rate_standard_deviation(firing_rate)
+
+    return fr_mean / fr_std
