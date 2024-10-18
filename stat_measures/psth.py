1eb46154e6a4b364ae6b0785e5a5ee27aba75498
Thanatorika <renwanginmath@gmail.com>
2024-10-18 21:40:53 +0800

    241018

---
+++ stat_measures/psth.py
@@ -0,0 +1,10 @@
+"""Creator: Ren Wang.
+Last edited: 2024-Oct-18 by RW.
+
+Code utilities for computing Peristimulus Time Histograms (PSTH) from spike bins.
+"""
+
+import sys
+sys.path.append("..")
+
+from data_utils import vr_utils as vru
